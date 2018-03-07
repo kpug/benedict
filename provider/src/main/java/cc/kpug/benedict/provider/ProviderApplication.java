8bf8d16a808acce0a562bf5896833f1fcf969838
before30 <before30@gmail.com>
2018-03-08 02:00:36 +0900

    Add health check actuator

--- provider/src/main/java/cc/kpug/benedict/provider/ProviderApplication.java
+++ provider/src/main/java/cc/kpug/benedict/provider/ProviderApplication.java
@@ -4,21 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by before30 on 08/03/2018.
  */
 @SpringBootApplication
-@RestController
 @Slf4j
 public class ProviderApplication implements CommandLineRunner {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "provider";
-    }
 
     @Override
     public void run(String... args) throws Exception {
---
+++ provider/src/main/java/cc/kpug/benedict/provider/conf/package-info.java
@@ -0,0 +1,1 @@
+package cc.kpug.benedict.provider.conf;
