baae5946657c3663d469b2ff82feda9cea0164c2
before30 <before30@gmail.com>
2018-03-08 01:22:48 +0900

    Add provider base code

---
+++ provider/src/main/java/cc/kpug/benedict/provider/ProviderApplication.java
@@ -0,0 +1,31 @@
+package cc.kpug.benedict.provider;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by before30 on 08/03/2018.
+ */
+@SpringBootApplication
+@RestController
+@Slf4j
+public class ProviderApplication implements CommandLineRunner {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "world";
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("hello world!");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}
